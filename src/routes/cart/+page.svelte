a9288ea929c5b8d88e31431c25bda024b55d3d8d
william <wayleemh@gmail.com>
2024-02-20 17:54:37 -0500

    work on cart page

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,1 +1,19 @@
-<span class="text-black">placeholder cart</span>
+<script lang="ts">
+	import { get } from 'svelte/store';
+	import { cartItems } from '../../cart';
+
+	let cart = get(cartItems);
+
+	cartItems.subscribe((newCart) => {
+		cart = newCart;
+	});
+</script>
+
+<div>
+	<span class="text-black">Placeholder cart</span>
+	{#each cart as item}
+		<div class="text-black">
+			{item.product.sku}
+		</div>
+	{/each}
+</div>
